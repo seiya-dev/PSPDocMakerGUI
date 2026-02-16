518769b896fba76e3f4b38e04b2d6c191a65def4
seiya-git <seiya-git@users.noreply.github.com>
2026-02-16 12:15:29 +0300

    Add *.tmp to .gitignore and update build.bat zip output path

--- build.bat
+++ build.bat
@@ -8,6 +8,6 @@ pyinstaller --onefile --noconsole --add-data "pspdocmaker;pspdocmaker" "pspdocma
 del pspdocmaker_gui.spec
 
 del /f /q dist/pspdocmaker_gui.zip 2>nul
-7z a -tzip pspdocmaker_gui.zip dist/pspdocmaker_gui.exe
+7z a -tzip dist/pspdocmaker_gui.zip dist/pspdocmaker_gui.exe
 
 pause
