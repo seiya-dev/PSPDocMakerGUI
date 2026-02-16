17e769c4effd5dab564cc94b6bef04542b27bcd2
seiya-git <seiya-git@users.noreply.github.com>
2026-02-16 10:39:13 +0300

    Move build directory removal before PyInstaller execution

--- build.bat
+++ build.bat
@@ -2,10 +2,9 @@
 
 %~d0
 cd %~dp0
+rmdir "build" /s /q
 rmdir "pspdocmaker/__pycache__" /s /q
 pyinstaller --onefile --noconsole --add-data "pspdocmaker;pspdocmaker" "pspdocmaker_gui.py"
 del pspdocmaker_gui.spec
-rmdir "build" /s /q
-rmdir "pspdocmaker/__pycache__" /s /q
 
 pause
