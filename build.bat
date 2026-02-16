bed234af5a81c7ef9d380863b82085567879241c
seiya-git <seiya-git@users.noreply.github.com>
2026-02-16 10:58:02 +0300

    Add zip packaging in build.bat and ignore .spec files in .gitignore

--- build.bat
+++ build.bat
@@ -7,4 +7,7 @@ rmdir "pspdocmaker/__pycache__" /s /q
 pyinstaller --onefile --noconsole --add-data "pspdocmaker;pspdocmaker" "pspdocmaker_gui.py"
 del pspdocmaker_gui.spec
 
+del /f /q dist/pspdocmaker_gui.zip 2>nul
+7z a -tzip pspdocmaker_gui.zip dist/pspdocmaker_gui.exe
+
 pause
